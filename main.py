45d2ad8601bb3cb0caf9b5818d7aeba73309b675
jean <zyh0192837465@outloo.com>
2025-05-20 23:22:40 +0800

    第四次提交（重构）

--- main.py
+++ main.py
@@ -4,10 +4,19 @@ from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.uix.label import Label
 from kivy.core.image import Image as ComcoreImage
-
+from kivy.uix.floatlayout import FloatLayout
 
 # 确保版本
 kivy_version = "2.1.0"
+
+
+class Widget(FloatLayout):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def loading_file(self):
+        pass
 
 
 # App入口
@@ -19,12 +28,15 @@ class Interface(App):
         Window.size = (400, 800)
         # 设置窗口颜色RGB(white Color)
         Window.clearcolor = (1, 1, 1, 1)
-        
 
     # 返回根控件
     def build(self):
-        return Builder.load_file(
+        # 加载kv文件
+        Builder.load_file(
             "/Users/jean/Desktop/python项目实战/CommunicationApplication/controls/widget.kv"
         )
+        return Widget()
+
+
 if __name__ == "__main__":
     Interface().run()
